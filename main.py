dedda6f4d4715ec2587bd38f20d8203fe9a846f1
Nathan Maynes <967457+nmaynes@users.noreply.github.com>
2020-12-06 12:30:58 -0500

    Add ratio comparison

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def compare_hamming(str1, str2):
     if len(str1) == len(str2):
         return Levenshtein.hamming(str1, str2)
     else:
-        raise ValueError("Each string must be of equal length")
+        # raise ValueError("Each string must be of equal length")
+        return -1
 
 
 def compare_jaro(str1, str2):
@@ -21,14 +22,17 @@ def compare_jaro_winkler(str1, str2):
     return Levenshtein.jaro_winkler(str1, str2)
 
 
-# Press the green button in the gutter to run the script.
+def compare_ratio(str1, str2):
+    return Levenshtein.ratio(str1, str2)
+
+
 if __name__ == '__main__':
     name_pairs = [
         ('bart simpson', 'bartholomew simpson'),
         ('lisa simpson', 'Lisa Simpson'),
         ('homer simpson', 'mr. homer simpson'),
         ('marge simpson', 'simpson, marge'),
-        ('Rabbi Hyman Krustofsky', ''),
+        ('honorable rabbi krustofsky', 'Rabbi Hyman Krustofsky'),
         ('crazy cat lady', 'Crazy Cat Lady'),
         ('jess lovejoy', 'Jessica Lovejoy'),
         ('booberella', 'Booberella'),
@@ -51,11 +55,16 @@ if __name__ == '__main__':
         levenshtein_dist = compare_levenshtein(first, second)
         jaro_dist = compare_jaro(first, second)
         jaro_winkler = compare_jaro_winkler(first, second)
+        hamming = compare_hamming(first, second)
+        ratio_dist = compare_ratio(first, second)
         print('{:^25} - {:^25}'.format(first, second))
         print(f'{"=" * 53}')
         print('{:25} | {:25}'.format('Levenshtein Distance', levenshtein_dist, align='right'))
         print('{:25} | {:25.2%}'.format('Jaro Distance', jaro_dist, align='right'))
         print('{:25} | {:25.2%}'.format('Jaro-Winkler Distance', jaro_winkler, align='right'))
+        print('{:25} | {:25}'.format('Hamming Distance', hamming, align='right'))
+        print('{:25} | {:25.2%}'.format('Ratio Distance', ratio_dist, align='right'))
+
         print('\n\n')
 
 
